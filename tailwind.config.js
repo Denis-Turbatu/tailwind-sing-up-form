a7f34e16c47619a3fc28fbe737b5e32ae942373a
Denis-Turbatu <turbatudenis81@gmail.com>
2024-03-22 15:56:00 +0100

    feat: mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      ...require('tailwindcss/colors'),
+      "charcoal": "hsl(235, 18%, 26%)",
+      "dark-slate": "hsl(234, 29%, 20%)",
+      "tomato": "hsl(4, 100%, 67%)",
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
