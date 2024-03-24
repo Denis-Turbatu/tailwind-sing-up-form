9ad318cd98415d32cd04cd2f59faa856c0ca27c8
Denis-Turbatu <turbatudenis81@gmail.com>
2024-03-24 15:03:35 +0100

    feat: gradient color to buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     colors: {
-      ...require('tailwindcss/colors'),
-      "charcoal": "hsl(235, 18%, 26%)",
+      ...require("tailwindcss/colors"),
+      charcoal: "hsl(235, 18%, 26%)",
       "dark-slate": "hsl(234, 29%, 20%)",
-      "tomato": "hsl(4, 100%, 67%)",
+      tomato: "hsl(4, 100%, 67%)"
     },
     extend: {
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
